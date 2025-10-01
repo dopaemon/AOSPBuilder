1635216a9bc300de7e24a8a52b512bca4c8f70c4
dopaemon <polarisdp@gmail.com>
2025-10-02 04:23:20 +0700

    .
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- build.sh
+++ build.sh
@@ -52,3 +52,27 @@ else
     echo "Missing File !!!"
     exit 1
 fi
+
+# ============================================================
+# Xiaomi 12T Pro
+cd "$PWDIR/packages/apps/Settings"
+git reset --hard HEAD
+
+sed -i 's/Nothing Phone 2/Xiaomi 12T Pro/g' res/values/lunaris_strings.xml
+sed -i 's/Snapdragon® 8\+ Gen 1/Snapdragon® 8+ Gen 1/g' res/values/lunaris_strings.xml
+sed -i 's/4700 mAh/5000 mAh/g' res/values/lunaris_strings.xml
+sed -i 's/Amoled LTPO 120hz/Amoled 120hz/g' res/values/lunaris_strings.xml
+
+cd "$PWDIR"
+. build/envsetup.sh
+lunch lineage_diting-bp2a-user
+m lunaris -j"$(nproc --all)" | tee log.txt
+
+FILE=$(ls out/target/product/diting/Lunaris-AOSP-*-Community-*.zip 2>/dev/null | head -n 1)
+
+if [ -n "$FILE" ] && [ -f "$FILE" ]; then
+    mv "$FILE" "$OUTPUT/"
+else
+    echo "Missing File !!!"
+    exit 1
+fi
