643c6d6d7aaca94963fe0187f46ec0e16d68b87c
dopaemon <polarisdp@gmail.com>
2025-10-02 04:17:28 +0700

    .
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ PWDIR=$(pwd)
 OUTPUT=$PWDIR/../out
 mkdir -p "$OUTPUT"
 
+# ============================================================
 # Xiaomi 12S Build
 cd "$PWDIR/packages/apps/Settings"
 git reset --hard HEAD
@@ -19,7 +20,31 @@ cd "$PWDIR"
 lunch lineage_mayfly-bp2a-user
 m lunaris -j"$(nproc --all)" | tee log.txt
 
-FILE=$(ls out/target/product/mayfly/Lunaris-AOSP-mayfly-Community-*.zip 2>/dev/null | head -n 1)
+FILE=$(ls out/target/product/mayfly/Lunaris-AOSP-*-Community-*.zip 2>/dev/null | head -n 1)
+
+if [ -n "$FILE" ] && [ -f "$FILE" ]; then
+    mv "$FILE" "$OUTPUT/"
+else
+    echo "Missing File !!!"
+    exit 1
+fi
+
+# ============================================================
+# Xiaomi 12S Pro
+cd "$PWDIR/packages/apps/Settings"
+git reset --hard HEAD
+
+sed -i 's/Nothing Phone 2/Xiaomi 12S Pro/g' res/values/lunaris_strings.xml
+sed -i 's/Snapdragon® 8\+ Gen 1/Snapdragon® 8+ Gen 1/g' res/values/lunaris_strings.xml
+sed -i 's/4700 mAh/4600 mAh/g' res/values/lunaris_strings.xml
+sed -i 's/Amoled LTPO 120hz/Amoled 120hz/g' res/values/lunaris_strings.xml
+
+cd "$PWDIR"
+. build/envsetup.sh
+lunch lineage_unicorn-bp2a-user
+m lunaris -j"$(nproc --all)" | tee log.txt
+
+FILE=$(ls out/target/product/unicorn/Lunaris-AOSP-*-Community-*.zip 2>/dev/null | head -n 1)
 
 if [ -n "$FILE" ] && [ -f "$FILE" ]; then
     mv "$FILE" "$OUTPUT/"
